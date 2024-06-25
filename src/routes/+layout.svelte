bb6b025a26d3005b4ad1c21921aea7f153793676
Aquon Bovell <aquon_bovell@outlook.com>
2024-06-25 19:14:40 -0400

    added base styles

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import Header from '$lib/Header.svelte';
+	import '../app.css';
+</script>
+
+<div class="dark:bg-black dark:text-white px-4">
+	<div class="container mx-auto">
+		<Header />
+		<main>
+			<slot />
+		</main>
+	</div>
+</div>
