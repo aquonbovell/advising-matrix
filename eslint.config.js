6eb0d8df8f343b81f326afbad95c9a510227ab74
phrogwrld <aaronah02@icloud.com>
2024-06-26 00:50:18 -0400

    chore: update configs and dependencies

--- eslint.config.js
+++ eslint.config.js
@@ -1,33 +1,46 @@
-import js from '@eslint/js';
-import ts from 'typescript-eslint';
-import svelte from 'eslint-plugin-svelte';
-import prettier from 'eslint-config-prettier';
 import globals from 'globals';
+import js from '@eslint/js';
+import prettier from 'eslint-plugin-prettier/recommended';
+import svelte from 'eslint-plugin-svelte';
+import ts from 'typescript-eslint';
 
-/** @type {import('eslint').Linter.FlatConfig[]} */
-export default [
+export default ts.config(
 	js.configs.recommended,
 	...ts.configs.recommended,
 	...svelte.configs['flat/recommended'],
-	prettier,
 	...svelte.configs['flat/prettier'],
+	prettier,
 	{
 		languageOptions: {
-			globals: {
-				...globals.browser,
-				...globals.node
-			}
+			globals: { ...globals.browser, ...globals.es2017 }
+		}
+	},
+	{
+		files: ['**/*.cjs', '**/*.mjs'],
+		languageOptions: {
+			globals: { ...globals.node }
 		}
 	},
 	{
 		files: ['**/*.svelte'],
 		languageOptions: {
-			parserOptions: {
-				parser: ts.parser
-			}
+			parserOptions: { parser: ts.parser }
 		}
 	},
 	{
-		ignores: ['build/', '.svelte-kit/', 'dist/']
-	}
-];
+		rules: {
+			'@typescript-eslint/no-unused-vars': [
+				'warn',
+				{
+					argsIgnorePattern: '^_',
+					caughtErrorsIgnorePattern: '^_',
+					varsIgnorePattern: '^_'
+				}
+			]
+		}
+	},
+	{
+		rules: { 'prettier/prettier': 'warn' }
+	},
+	{ ignores: ['**/.svelte-kit', 'build/', 'dist/'] }
+);
