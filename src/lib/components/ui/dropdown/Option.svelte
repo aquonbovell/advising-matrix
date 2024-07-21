ff520de156235ebb9099ee413d746c2efb144b32
phrogwrld <aaronah02@icloud.com>
2024-07-20 22:03:41 -0400

    feat: Add Option and Select components for dropdown functionality
    This commit adds the Option and Select components to enable dropdown functionality in the UI. The Option component represents an individual dropdown option, while the Select component is the main dropdown container. These components provide the ability to select an option, handle click events, and update the selected value.

---
+++ src/lib/components/ui/dropdown/Option.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import CheckMarkIcon from '$lib/components/icons/CheckIcon.svelte';
+	import type { HTMLOptionAttributes } from 'svelte/elements';
+
+	export let value: HTMLOptionAttributes['value'];
+	export let disabled: boolean = false;
+
+	let selected: boolean = false;
+</script>
+
+<button
+	type="button"
+	data-value={value}
+	data-type={typeof value}
+	class="flex w-full items-center px-4 py-2 text-left hover:bg-gray-100 focus:bg-gray-100 focus:outline-none enabled:hover:cursor-pointer disabled:cursor-not-allowed disabled:bg-gray-50"
+	{disabled}
+>
+	<slot />
+</button>
