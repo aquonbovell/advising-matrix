8b878408650c4ca5e593002c6ff2b24a47e2a1e0
Aquon Bovell <aquon_bovell@outlook.com>
2024-07-21 17:43:19 -0400

    feat: Add courses page with server-side data loading
    This commit adds a new page for displaying courses. The page is implemented using Svelte and fetches the course data from the server using server-side rendering. The courses are fetched from the database and ordered by level and code. The page also includes a table for displaying the course information, including the code, name, level, and an action column. If no courses are found, a message is displayed indicating that no courses were found. This change improves the user experience by providing a way to view and interact with the courses in a structured manner.

---
+++ src/routes/(protected)/courses/+page.server.ts
@@ -0,0 +1,9 @@
+import type { PageServerLoad } from './$types';
+import { db } from '$lib/db';
+
+export const load = (async () => {
+	const courses = await db.selectFrom('Course').orderBy('Course.level asc').orderBy("Course.code asc").selectAll().execute();
+	return {
+		courses
+	};
+}) satisfies PageServerLoad;
