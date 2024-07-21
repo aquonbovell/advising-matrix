ed7cc8e1fba90cff233a1564bb37ca6646fd42fb
Aquon Bovell <aquon_bovell@outlook.com>
2024-07-21 17:27:27 -0400

    refactor: Update Sidebar component to align user role text to the left

--- src/lib/components/ui/Avatar.svelte
+++ src/lib/components/ui/Avatar.svelte
@@ -5,5 +5,7 @@
 <div
 	class="relative flex h-10 w-10 items-center justify-center rounded-full bg-blue-100 text-blue-600"
 >
-	<span class=" absolute font-semibold uppercase leading-none">{name[0]}</span>
+	<span class=" absolute font-semibold uppercase leading-none"
+		>{name === null ? 'Anonymous' : name[0]}</span
+	>
 </div>
