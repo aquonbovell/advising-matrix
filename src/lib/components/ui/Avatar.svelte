98863af7a95a8866cd592c43b041bbce4c8acdd1
Aquon Bovell <aquon_bovell@outlook.com>
2024-07-21 17:02:10 -0400

    refactor: Update Avatar component to position name label

--- src/lib/components/ui/Avatar.svelte
+++ src/lib/components/ui/Avatar.svelte
@@ -2,6 +2,8 @@
 	export let name: string;
 </script>
 
-<div class="flex h-10 w-10 items-center justify-center rounded-full bg-blue-100 text-blue-600">
-	<span class="text-lg font-semibold">{name[0]}</span>
+<div
+	class="relative flex h-10 w-10 items-center justify-center rounded-full bg-blue-100 text-blue-600"
+>
+	<span class=" absolute font-semibold uppercase leading-none">{name[0]}</span>
 </div>
