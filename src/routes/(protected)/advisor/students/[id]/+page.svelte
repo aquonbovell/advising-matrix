5503fef552947e220b16f31c3170881d85118430
Aquon Bovell <aquon_bovell@outlook.com>
2024-07-23 20:15:30 -0400

    refactor: Update student ID references to user ID in advisor students page

---
+++ src/routes/(protected)/advisor/students/[id]/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import type { PageData } from './$types';
+
+	export let data: PageData;
+</script>
+
+<h1>Student {data.student.id}</h1>
+
+<p>Student Name: {data.student.name}</p>
+<p>Student Email: {data.student.email}</p>
+<p>Student Role: {data.student.role}</p>
+<p>Student created_at {data.student.created_at}</p>
+<p>Student updated_at {data.student.updated_at}</p>
