08adbf355df0d9f5994ec4b5e7e156f5569f0b4f
Aquon Bovell <aquon_bovell@outlook.com>
2024-07-26 16:22:14 -0400

    refactor: Update advisor courses

--- src/routes/(protected)/advisor/courses/+page.svelte
+++ src/routes/(protected)/advisor/courses/+page.svelte
@@ -2,33 +2,10 @@
 	import type { PageData } from './$types';
 
 	export let data: PageData;
+
+	import DataTable from "./data-table.svelte";
 </script>
 
-<h2>All Courses</h2>
-{#if data.courses.length === 0}
-	<p>No courses available.</p>
-{:else}
-	<table>
-		<thead>
-			<tr>
-				<th>Code</th>
-				<th>Name</th>
-				<th>Level</th>
-				<th>Credits</th>
-			</tr>
-		</thead>
-		<tbody>
-			{#each data.courses as course}
-				<tr>
-					<td>{course.code}</td>
-					<td>{course.name}</td>
-					<td>{course.level}</td>
-					<td>{course.credits}</td>
-				</tr>
-			{/each}
-		</tbody>
-	</table>
-{/if}
-
-<style>
-</style>
+<div class="container mx-auto py-10">
+	<DataTable data={data} />
+</div>
