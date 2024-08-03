37ec3483dba32226396f8472b63e7fc5456e6271
Aquon Bovell <aquon_bovell@outlook.com>
2024-08-03 08:39:08 -0400

    refactor: Remove unnecessary code in student route
    The unused code in the student route file has been removed. This refactor improves code cleanliness and reduces potential confusion.

--- src/routes/(protected)/student/+page.server.ts
+++ src/routes/(protected)/student/+page.server.ts
@@ -1,11 +1,15 @@
 import type { PageServerLoad } from './$types';
 import { db } from '$lib/db';
+import { error } from '@sveltejs/kit';
 
 export const load = (async ({ locals }) => {
+	const userId = locals.user?.id;
+	if (!userId) throw error(401, 'Unauthorized');
+
 	const data = await db
 		.selectFrom('Student')
 		.select(['Student.id', 'Student.user_id'])
-		.where('Student.user_id', '=', `${locals.user?.id}`)
+		.where('Student.user_id', '=', userId)
 		.executeTakeFirst();
 	if (!data) {
 		return {
