f64a947039b1d548dfb617b7a3b857932f547fa5
Aquon Bovell <aquon_bovell@outlook.com>
2024-11-09 22:14:27 -0400

    feat: implement user and student pagination; add user deletion functionality and student token renewal

--- src/lib/actions/student.actions.ts
+++ src/lib/actions/student.actions.ts
@@ -1,6 +1,7 @@
 import { db } from '$lib/db';
+import { generateTokenWithExpiration } from '$lib/server/auth';
 import type { Grade, StudentCoursesWithUser } from '$lib/types';
-import { isValidUUID } from '$lib/utils';
+import { isCompleted, isValidUUID } from '$lib/utils';
 
 export async function fetchStudentCourses(
 	studentId: string
@@ -102,4 +103,148 @@ export async function updateStudentSuggestions(
 	}
 
 	return { status: 200, message: 'Student courses updated' };
+}
+
+export async function renewToken(studentId: string) {
+	if (!isValidUUID(studentId)) {
+		throw new Error('Invalid studentId');
+	}
+	const { expiresAt, token } = generateTokenWithExpiration();
+
+	const student = await db
+		.updateTable('Student')
+		.set({
+			invite_token: token,
+			invite_expires: new Date(expiresAt),
+			updated_at: new Date()
+		})
+		.where('id', '=', studentId)
+		.returning('id')
+		.executeTakeFirst();
+
+	return student;
+}
+
+export async function getStudentById(studentId: string) {
+	if (!isValidUUID(studentId)) {
+		throw new Error('Invalid studentId');
+	}
+
+	const student = await db
+		.selectFrom('Student')
+		.innerJoin('User', 'Student.user_id', 'User.id')
+		.select([
+			'Student.invite_token',
+			'Student.invite_expires',
+			'User.name',
+			'User.email',
+			'User.alternate_email'
+		])
+		.where('Student.id', '=', studentId)
+		.executeTakeFirst();
+
+	return student;
+}
+
+export async function overview(studentId: string) {
+	if (!isValidUUID(studentId)) {
+		throw new Error('Invalid studentId');
+	}
+
+	const degree = await db
+		.selectFrom('Student')
+		.where('Student.id', '=', studentId)
+		.select(['major_id', 'minor_id'])
+		.executeTakeFirst();
+
+	if (!degree) {
+		throw new Error('Student not found');
+	}
+
+	const majorDetails = await db
+		.selectFrom('Majors')
+		.innerJoin('MajorRequirements', 'Majors.id', 'MajorRequirements.majorId')
+		.where('Majors.id', 'in', [degree.major_id, degree.minor_id])
+		.select(['MajorRequirements.id', 'MajorRequirements.credits', 'MajorRequirements.level'])
+		.execute();
+	const minorDetails = await db
+		.selectFrom('Minors')
+		.innerJoin('MinorRequirements', 'Minors.id', 'MinorRequirements.minorId')
+		.where('Minors.id', 'in', [degree.major_id, degree.minor_id])
+		.select(['MinorRequirements.id', 'MinorRequirements.credits', 'MinorRequirements.level'])
+		.execute();
+
+	const studentCourses = await db
+		.selectFrom('StudentCourses')
+		.innerJoin('Courses', 'StudentCourses.courseId', 'Courses.id')
+		.where('StudentCourses.studentId', '=', studentId)
+		.select([
+			'Courses.id',
+			'Courses.name',
+			'Courses.credits',
+			'StudentCourses.grade',
+			'requirementId'
+		])
+		.execute();
+
+	const requirements = [...majorDetails, ...minorDetails].map((req) => ({
+		...req,
+		level: JSON.parse(req.level) as number[]
+	}));
+
+	const levelOne = requirements.filter((req) => req.level.includes(1) && req.level.length === 1);
+
+	const levelTwo = requirements.filter((req) => req.level.includes(2) && req.level.length === 1);
+
+	const levelThree = requirements.filter((req) => req.level.includes(3) && req.level.length === 1);
+
+	const forCredits = requirements.filter(
+		(req) => ![...levelOne, ...levelTwo, ...levelThree].includes(req)
+	);
+
+	const student = {
+		levelOne: {
+			credits: levelOne.reduce((acc, req) => acc + req.credits, 0),
+			completed: studentCourses
+				.filter((course) =>
+					levelOne.some(
+						(req) => req.id === course.requirementId && isCompleted(JSON.parse(course.grade))
+					)
+				)
+				.reduce((acc, course) => acc + course.credits, 0)
+		},
+		levelTwo: {
+			credits: levelTwo.reduce((acc, req) => acc + req.credits, 0),
+			completed: studentCourses
+				.filter((course) =>
+					levelTwo.some(
+						(req) => req.id === course.requirementId && isCompleted(JSON.parse(course.grade))
+					)
+				)
+				.reduce((acc, course) => acc + course.credits, 0)
+		},
+		levelThree: {
+			credits: levelThree.reduce((acc, req) => acc + req.credits, 0),
+			completed: studentCourses
+				.filter((course) =>
+					levelThree.some(
+						(req) => req.id === course.requirementId && isCompleted(JSON.parse(course.grade))
+					)
+				)
+				.reduce((acc, course) => acc + course.credits, 0)
+		},
+
+		forCredits: {
+			credits: forCredits.reduce((acc, req) => acc + req.credits, 0),
+			completed: studentCourses
+				.filter((course) =>
+					forCredits.some(
+						(req) => req.id === course.requirementId && isCompleted(JSON.parse(course.grade))
+					)
+				)
+				.reduce((acc, course) => acc + course.credits, 0)
+		}
+	};
+
+	return student;
 }
