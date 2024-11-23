79c99b05d83ab28bb0a74fcc7734f78114bd8ea0
Aquon Bovell <aquon_bovell@outlook.com>
2024-11-22 20:30:43 -0400

    feat: update database configuration; switch from local SQLite to environment-based Turso database URL

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -11,7 +11,7 @@ if (!env.TURSO_DATABASE_URL) throw new Error('TURSO_DATABASE_URL is not set');
 
 export const db = new Kysely<DB>({
 	dialect: new LibsqlDialect({
-		url: 'file:./testlocal.db',
+		url: env.TURSO_DATABASE_URL,
 		syncUrl: env.TURSO_DATABASE_URL,
 		authToken: env.TURSO_AUTH_TOKEN,
 		syncInterval: 60000
