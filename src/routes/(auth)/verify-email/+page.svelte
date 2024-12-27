38a7f153d08256e41676bf9c1539a1d7c4c08e3f
Aquon Bovell <aquon_bovell@outlook.com>
2024-12-27 05:15:38 -0400

    chore: simplify email verification component

--- src/routes/(auth)/verify-email/+page.svelte
+++ src/routes/(auth)/verify-email/+page.svelte
@@ -1,10 +1,10 @@
 <script lang="ts">
 	import VerifyEmailForm from '$lib/components/verify-email-form.svelte';
-	import type { ActionData, PageData } from './$types';
+	import type { ActionData } from './$types';
 
-	let { data, form }: { data: PageData; form: ActionData } = $props();
+	let { form }: { form: ActionData } = $props();
 </script>
 
 <div class="flex h-screen w-full items-center justify-center px-4">
-	<VerifyEmailForm {data} {form} />
+	<VerifyEmailForm {form} />
 </div>
